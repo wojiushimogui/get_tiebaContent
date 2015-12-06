74d09d7ef2c068627250690ba4d99c4aa17cdd1e
wuranghao <154943046@qq.com>
2015-12-06 22:02:24 +0800

    get tieba content

--- get_tiebaContent.py
+++ get_tiebaContent.py
@@ -31,11 +31,19 @@ class Tool:
 
 #定义一个类
 class BaiduTieBa:
-	#初始化，传入地址，以及是否只看楼主的参数
-	def __init__(self,url,seeLZ):
+	#初始化，传入地址，以及是否只看楼主的参数,floorTag:为1就是往文件中写入楼层分隔符
+	def __init__(self,url,seeLZ,floorTag):
 		self.url=url
 		self.seeLZ="?see_lz="+str(seeLZ)
 		self.tool=Tool()
+		#全局的文件变量
+		self.file=None
+		#默认标题，如果没有获取到网页上帖子的标题，此将作为文件的名字
+		self.defultTitle="百度贴吧"
+		#是否往文件中写入楼与楼的分隔符
+		self.floorTag=floorTag
+		#楼层序号
+		self.floor=1
 	#根据传入的页码来获取帖子的内容太
 	def getPageContent(self,pageNum):
 		url=self.url+self.seeLZ+"&pn="+str(pageNum)
@@ -94,18 +102,59 @@ class BaiduTieBa:
 		pattern=re.compile(r'<div id="post_content_.*?>(.*?)</div>',re.S)
 		items=re.findall(pattern,content)
 		floor=1
+		contents=[]
 		for item in items:
-			print floor,u"楼-------------------------------------------\n"
-			print self.tool.replace(item)
-			floor+=1
-		return items
+			#将每个楼层的内容进行去除标签处理，同时在前后加上换行符
+			tempContent="\n"+self.tool.replace(item)+"\n"
+			contents.append(tempContent.encode("utf-8"))
+			#测试输出
+			#print floor,u"楼-------------------------------------------\n"
+			#print self.tool.replace(item)
+			#floor+=1
+		return contents
+	#将内容写入文件中保存
+	def writedata2File(self,contents):
+		for item in contents:
+			if self.floorTag=="1":
+				#楼与楼之间的分隔符
+				floorLine=u"\n"+str(self.floor)+u"-------------------\n"
+				self.file.write(floorLine)
+			print u"正在往文件中写入第"+str(self.floor)+u"楼的内容"
+			self.file.write(item)
+			self.floor+=1
+	#根据获取网页帖子的标题来在目录下建立一个同名的.txt文件
+	def newFileAccTitle(self,title):
+		if title is not None:
+			self.file=open(title+".txt","w+")
+		else:
+			self.file=open(defultTitle+".txt","w+")
+	#写一个抓取贴吧的启动程序
+	def start(self,pageNum):
+		#先获取html代码
+		content=self.getPageContent(pageNum)
+		#第二步：开始解析，获取帖子的标题和作者
+		title=self.getPageTitle(pageNum)
+		#根据title建立一个即将用于写入的文件
+		self.newFileAccTitle(title)
+		author=self.getPageAuthor(pageNum)
+		#第三步：获取帖子各个楼层的内容
+		contents=self.getContent(pageNum)
+		#第四步：开始写入文件中
+		try:
+			self.writedata2File(contents)
+		except IOError,e:
+			print "写入文件发生异常，原因"+e.message
+		finally:
+			print "写入文件完成！"
 
-
+#测试代码如下：
 #url=raw_input("raw_input:")
 url="http://www.tieba.baidu.com/p/4197307435"
-seeLZ=input("input a number:")
+seeLZ=input("input see_lz:")
 pageNum=input("input pageNum:")
-baidutieba=BaiduTieBa(url,seeLZ)#实例化一个对象
+floorTag=raw_input("input floorTag:")
+baidutieba=BaiduTieBa(url,seeLZ,floorTag)#实例化一个对象
+baidutieba.start(pageNum)
 #content=baidutieba.getPageContent(pageNum)#调用函数
 #开始解析得到帖子标题
 #baidutieba.getPageTitle(1)
@@ -113,4 +162,4 @@ baidutieba=BaiduTieBa(url,seeLZ)#实例化一个对象
 #baidutieba.getPageAuthor(1)
 #baidutieba.getPageTotalPageNum(1)
 #解析帖子中的内容
-baidutieba.getContent(pageNum)
+#baidutieba.getContent(pageNum)
